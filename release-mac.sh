c8e251020974e0edfa4f1d67de562227ed9d859c
Christian Clare <c.clare@gmail.com>
2025-09-13 16:02:36 -0400

    Create Mac release script

---
+++ release-mac.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+echo "Building PTUI for release..."
+cargo build --release
+
+VERSION=$(grep '^version = ' Cargo.toml | sed 's/version = "\(.*\)"/\1/')
+
+if [ -z "$VERSION" ]; then
+    echo "Error: Could not extract version from Cargo.toml"
+    exit 1
+fi
+
+TARBALL="ptui-${VERSION}-mac-x86_64.tar.gz"
+
+echo "Creating tarball: $TARBALL"
+tar -czf "$TARBALL" -C target/release ptui
+
+echo "Release tarball created: $TARBALL"
+
